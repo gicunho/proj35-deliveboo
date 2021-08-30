daa486f26078e7d338fbcbb6ab9c78777301440f
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-08-30 15:41:38 +0200

    Add relationships between orders and dishes

--- app/Dish.php
+++ app/Dish.php
@@ -4,10 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Restaurant;
+use App\Order;
+
 
 class Dish extends Model
 {
     public function restaurant(){
         return $this->belongsTo(Restaurant::class);
+    }
+
+    public function orders(){
+        return $this->belongsToMany(Order::class);
     }
 }
--- app/Order.php
+++ app/Order.php
@@ -4,10 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Restaurant;
+use App\Dish;
 
 class Order extends Model
 {
     public function restaurant(){
         return $this->belongsTo(Restaurant::class);
+    }
+
+    public function dishes(){
+        return $this->belongsToMany(Dish::class);
     }
 }
