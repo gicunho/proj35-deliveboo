1854f50efc12c83e92d360f8ba2f9692d6db0655
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-08-30 16:50:32 +0200

    Add seeders to database seeder array

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([
-             CategorySeeder::class,
-            
+            CategorySeeder::class,
+            DishSeeder::class,
+            OrderSeeder::class,
+            RestaurantSeeder::class,
             ]);
     }
 }
