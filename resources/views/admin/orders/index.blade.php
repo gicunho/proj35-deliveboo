403115649b79279d1f846f8b8caaf29bc3f95d63
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-09-14 16:27:44 +0200

    Checkout view fix

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -9,21 +9,6 @@
         </div>
 
         <h1 class="text-center">I tuoi ordini</h1>
-<<<<<<< HEAD
-
-        <div class="d-flex flex-wrap justify-content-center">
-            <div class="card shadow p-3 mb-5 bg-white rounded col-md-5 m-3 p-3" v-for="order in orders" v-if="order.user_id ===  {{ $user->id }}">
-                <div class="d-flex">
-                    <h4 class="mr-2">@{{ order.name }}</h4>
-                    <h4>@{{ order.surname }}</h4>
-                </div>
-                <h6>Indirizzo: @{{ order.address }}</h6>
-                <h6>Telefono: @{{ order.phone_number }}</h6>
-                <h6>Prezzo: @{{ order.total_price }}$</h6>
-                <h6>Data: @{{order.date}}</h6>
-            </div>
-        </div>
-=======
           
           <table class="table">
             <thead class="thead-light">
@@ -45,6 +30,5 @@
               </tr>
             </tbody>
           </table>
->>>>>>> d97f6955a41eb0edcb7d8fa3f1bf446be3c3df7c
     </div>
 @endsection
