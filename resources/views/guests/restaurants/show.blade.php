103dc3f834119b0bf5ab04fbf3a59d04a488bb0c
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-09-14 12:59:05 +0200

    Fixed responsive and sticky cart in restaurants.show

--- resources/views/guests/restaurants/show.blade.php
+++ resources/views/guests/restaurants/show.blade.php
@@ -28,18 +28,20 @@
                                 <h4>Il nostro menú: </h4>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-10 ml-4 col-sm-6 {{-- mr-xs-1 --}} col-md-5 mx-md-4 col-lg-6 {{-- mr-lg-3 --}} col-xl-5 mx-xl-4 p-0 dish" v-for="(dish, index) in dishes" v-if="dish.user_id == {{$user->id}}">
-                                <div class="description">
-                                    <div>
-                                        <h4 class="dish_name">@{{dish.name}}</h4>
-                                        <p class="dish_price">Prezzo: € @{{dish.price}}</p>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 mx-xl-4 mx-lg-3 mx-md-4 mx-sm-3 mx-0 col-sm-5 col-md-5 col-lg-5 col-xl-5 p-0 dish" v-for="(dish, index) in dishes" v-if="dish.user_id == {{$user->id}}">
+                                    <div class="description">
+                                        <div>
+                                            <h4 class="dish_name">@{{dish.name}}</h4>
+                                            <p class="dish_price">Prezzo: € @{{dish.price}}</p>
+                                        </div>
+                                        <div class="button">
+                                            <i class="fa fa-plus plus ml-2" aria-hidden="true" @click="addToCart(dish, {{$user->id}})"></i>
+                                        </div>
                                     </div>
-                                    <div class="button">
-                                        <i class="fa fa-plus plus ml-2" aria-hidden="true" @click="addToCart(dish, {{$user->id}})"></i>
-                                    </div>
+                                    <div class="dish_image" :style="{ backgroundImage: 'url(\'storage/' + dish.image + '\')' }"></div>
                                 </div>
-                                <img :src="'/storage/' + dish.image" class="dish_image" width="150" height="130" alt="">
                             </div>
                         </div>
                     </div>
@@ -47,47 +49,49 @@
             </div>
 
             <!-- Cart -->
-            <div class="col-sm-12 col-lg-4 position-relative pr-4 mt-5">
-                <div class="row sticky-top">
-                    <div class="col-12 cart">
-                        <a href="{{route('orders.create', $user->id)}}" class="btn active btn-brand-secondary text-white btn-block" v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}">Vai alla cassa</a>
-                        <button disabled class="btn btn-brand-secondary text-white btn-block" v-else>Vai alla cassa</button>
-                        <h4 v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}" class="mt-4">Il tuo ordine:</h4>
-                        <ul class='px-0' v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}">
-                            <li v-for="(dish, index) in cart" v-if="dish.user_id == {{$user->id}}">
-                                <div class="row d-flex justify-content-around">
-                                    <div class="col-3 col-md-4 col-lg-3 py-1 px-0">
-                                        <i class="fa fa-minus-circle" aria-hidden="true" @click="removeFromCart(dish,index)"></i>
-                                        <span class="quantity px-1">@{{dish.quantity}}</span>
-                                        <i class="fa fa-plus-circle" aria-hidden="true" @click="addToCart(dish, {{$user->id}})"></i>
-                                    </div> 
-                                    <div class="col-6 col-md-3 col-lg-5 py-1 px-0">
-                                        <span>@{{dish.name}}</span>
+            <div class="col-12 col-lg-4  pr-lg-4 px-0 mt-5">
+                <div class="container position-relative h-100">
+                    <div class="row sticky-top mx-0">
+                        <div class="col-12 cart">
+                            <a href="{{route('orders.create', $user->id)}}" class="btn active btn-brand-secondary text-white btn-block" v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}">Vai alla cassa</a>
+                            <button disabled class="btn btn-brand-secondary text-white btn-block" v-else>Vai alla cassa</button>
+                            <h4 v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}" class="mt-4">Il tuo ordine:</h4>
+                            <ul class='px-0' v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}">
+                                <li v-for="(dish, index) in cart" v-if="dish.user_id == {{$user->id}}">
+                                    <div class="row d-flex justify-content-around">
+                                        <div class="col-3 col-md-4 col-lg-3 py-1 px-0">
+                                            <i class="fa fa-minus-circle" aria-hidden="true" @click="removeFromCart(dish,index)"></i>
+                                            <span class="quantity px-1">@{{dish.quantity}}</span>
+                                            <i class="fa fa-plus-circle" aria-hidden="true" @click="addToCart(dish, {{$user->id}})"></i>
+                                        </div> 
+                                        <div class="col-6 col-md-3 col-lg-5 py-1 px-0">
+                                            <span>@{{dish.name}}</span>
+                                        </div>
+                                        <div class="col-2 col-md-2 col-lg-2 py-1 px-0">
+                                            <span>€ @{{dish.price}}</span>
+                                        </div>
+                                        <div class="col-1 col-md-1 col-lg-1 py-1 px-0">
+                                            <i class="fas fa-times-circle" @click="deleteDish(dish, index)"></i>
+                                        </div>
                                     </div>
-                                    <div class="col-2 col-md-2 col-lg-2 py-1 px-0">
-                                        <span>€ @{{dish.price}}</span>
+                                </li>
+                                <hr>
+                                <li>
+                                    <div class="row">
+                                        <div class="col-sm-3 offset-sm-7 col-md-6 col-lg-4 offset-md-2 offset-lg-5 py-1">
+                                            <h3>Totale: </h3>
+                                        </div>
+                                        <div class="col-sm-2 col-md-4 col-lg-3 py-1 px-0">
+                                            <h3>€ @{{total_price}}</h3>
+                                        </div>
                                     </div>
-                                    <div class="col-1 col-md-1 col-lg-1 py-1 px-0">
-                                        <i class="fas fa-times-circle" @click="deleteDish(dish, index)"></i>
-                                    </div>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-brand-danger text-white btn-block" v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}" @click="emptyCart">Svuota il carrello</button>
+                            <div class="row d-flex text-center align-items-center" v-else>
+                                <div class='col-12 pt-4'>
+                                    Il carrello è vuoto
                                 </div>
-                            </li>
-                            <hr>
-                            <li>
-                                <div class="row">
-                                    <div class="col-sm-3 offset-sm-7 col-md-6 col-lg-4 offset-md-2 offset-lg-5 py-1">
-                                        <h3>Totale: </h3>
-                                    </div>
-                                    <div class="col-sm-2 col-md-4 col-lg-3 py-1 px-0">
-                                        <h3>€ @{{total_price}}</h3>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                        <button type="button" class="btn btn-brand-danger text-white btn-block" v-if="cart.length > 0 && cart[0].user_id == {{$user->id}}" @click="emptyCart">Svuota il carrello</button>
-                        <div class="row d-flex text-center align-items-center" v-else>
-                            <div class='col-12 pt-4'>
-                                Il carrello è vuoto
                             </div>
                         </div>
                     </div>
