f9b5b2baedee20ce72085995e2cbca3d60773aa7
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-09-14 15:16:34 +0200

    Change design to cart, implement confirm to empty cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -203,13 +203,15 @@ const app = new Vue({
             localStorage.setItem('total_price', JSON.stringify(this.total_price));
         },
         emptyCart() {
-            this.cart.forEach((dish, ind) => {
-                this.cart[ind].quantity = 1; //dish
-            })
-            this.cart = [];
-            this.total_price = 0;
-            localStorage.setItem('cart', JSON.stringify(this.cart));
-            localStorage.setItem('total_price', JSON.stringify(this.total_price));
+            if (confirm('Confermi di voler svuotare il carrello?')) {
+                this.cart.forEach((dish, ind) => {
+                    this.cart[ind].quantity = 1; //dish
+                })
+                this.cart = [];
+                this.total_price = 0;
+                localStorage.setItem('cart', JSON.stringify(this.cart));
+                localStorage.setItem('total_price', JSON.stringify(this.total_price));
+            }
         },
         resetCategories() {
             this.selectedInApi = '';
